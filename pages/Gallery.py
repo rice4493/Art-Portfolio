44950ab47d70f907bea70d2bdd56a9f9a1656977
Yukta <yukta.noela@outlook.com>
2023-06-28 20:22:02 +0530

    Adding to gallery

--- pages/Gallery.py
+++ pages/Gallery.py
@@ -1,13 +1,20 @@
 import streamlit as st
-import pandas
+import pandas as pd
+import os
 
 st.title("The Easel Gap")
 st.header("Gallery")
 st.subheader("A quick look into my previous work...")
 
-df = pandas.read_csv('images.csv')
+csv_path = os.path.join(os.path.dirname(__file__), 'images.csv')
+df = pd.read_csv(csv_path)
+
+image_folder = os.path.join(os.path.dirname(__file__), 'D:\Art Portfolio-web', 'images')
+
+rows_with_zero_image = df[df['image'] == '0']
 
 for index, row in df.iterrows():
     st.caption(row['title'])
     st.write(row['description'])
-    st.image(row['image'])
+    image_path = os.path.join(image_folder, row['image'])
+    st.image(image_path)
